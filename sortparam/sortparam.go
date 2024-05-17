85508274499c3edc808174f990dd6ced58c9cd07
denilany <denilanyonyi1@gmail.com>
2024-05-17 11:19:39 +0300

    Modified the sortparam file to sort arguments passed in the terminal, based on their arrangements according to ASCII table

--- sortparam/sortparam.go
+++ sortparam/sortparam.go
@@ -1,20 +1,31 @@
 package main
 
-import "fmt"
+import (
+	"os"
 
-func Sorted(arr []int) {
+	"github.com/01-edu/z01"
+)
+
+func Sorted(arr []string) []string {
 	n := len(arr)
 	for i := 0; i < n-1; i++ {
-		for j := 0; j < n-1; j++ { // use this: for j := 0; j < n-1; j++ if it is in descening order
+		for j := 0; j < n-1; j++ { // use this: (for j := 0; j < n-1; j++), if it is in descending order
 			if arr[j] > arr[j+1] {
 				arr[j], arr[j+1] = arr[j+1], arr[j]
 			}
 		}
 	}
-	fmt.Println(arr)
+	return arr
+}
+
+func print(arr []string) {
+	for _, str := range arr {
+		z01.PrintRune(rune(str[0]))
+		z01.PrintRune('\n')
+	}
 }
 
 func main() {
-	arr := []int{2, 4, 1, 7, 5, 3, 6}
-	Sorted(arr)
+	args := os.Args[1:]
+	print(Sorted(args))
 }
