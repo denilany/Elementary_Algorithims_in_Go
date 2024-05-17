799434a7f7a99b1a059d151bb1249c89290015f8
denilany <denilanyonyi1@gmail.com>
2024-05-17 10:54:45 +0300

    Added a sorting algorithim to sort an integer

--- params/go.mod
+++
@@ -1,5 +0,0 @@
-module params
-
-go 1.22.1
-
-require github.com/01-edu/z01 v0.1.0 // indirect
---
+++ sortparam/sortparam.go
@@ -0,0 +1,20 @@
+package main
+
+import "fmt"
+
+func Sorted(arr []int) {
+	n := len(arr)
+	for i := 0; i < n-1; i++ {
+		for j := 0; j < n-1-1; j++ { // use this: for j := 0; j < n-1; j++ if it is in descening order
+			if arr[j] > arr[j+1] {
+				arr[j], arr[j+1] = arr[j+1], arr[j]
+			}
+		}
+	}
+	fmt.Println(arr)
+}
+
+func main() {
+	arr := []int{2, 4, 1, 7, 5, 3, 6}
+	Sorted(arr)
+}
