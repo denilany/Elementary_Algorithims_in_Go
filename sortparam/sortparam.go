0a5640aba38026fb6865f0b8887b1f2a9ac4c963
denilany <denilanyonyi1@gmail.com>
2024-05-17 11:00:49 +0300

    Added a sorting algorithim to sort an integer

--- sortparam/sortparam.go
+++ sortparam/sortparam.go
@@ -5,7 +5,7 @@ import "fmt"
 func Sorted(arr []int) {
 	n := len(arr)
 	for i := 0; i < n-1; i++ {
-		for j := 0; j < n-1-1; j++ { // use this: for j := 0; j < n-1; j++ if it is in descening order
+		for j := 0; j < n-1; j++ { // use this: for j := 0; j < n-1; j++ if it is in descening order
 			if arr[j] > arr[j+1] {
 				arr[j], arr[j+1] = arr[j+1], arr[j]
 			}
