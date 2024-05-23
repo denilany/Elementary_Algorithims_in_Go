ca9b177749e3e5bf768d1567db7d882796c6092b
denilany <denilanyonyi1@gmail.com>
2024-05-23 11:28:03 +0300

    Is Power of 2: a program to check if a number is a power of 2. This solution is done using the logic of dividing the number by 2 repeatedly until it becomes 1

---
+++ ispowerof2/main.go
@@ -0,0 +1,64 @@
+package main
+
+import (
+	"os"
+
+	"github.com/01-edu/z01"
+)
+
+func main() {
+	if len(os.Args) != 2 {
+		return
+	}
+	arg := os.Args[1]
+	num := atoi(arg)
+	if num < 0 {
+		return
+	}
+	var status string
+	if isPowerOf2(num) {
+		status = "true"
+	} else {
+		status = "false"
+	}
+
+	printLn(status)
+}
+
+func atoi(s string) int {
+	var result int
+	for _, digit := range s {
+		if digit < '0' || digit > '9' {
+			return 0
+		}
+		result = result*10 + int(digit-'0')
+	}
+	return result
+}
+
+func isPowerOf2(n int) bool {
+	for n > 1 {
+		if n%2 != 0 {
+			return false
+		}
+		n /= 2
+	}
+	return true
+}
+
+func itoa(num int) string {
+	var str string
+	for num > 0 {
+		val := num % 10
+		str += string('0' + val)
+		num /= 10
+	}
+	return str
+}
+
+func printLn(s string) {
+	for _, ch := range s {
+		z01.PrintRune(ch)
+	}
+	z01.PrintRune('\n')
+}
