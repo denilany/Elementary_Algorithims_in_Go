ada628ce5ad14fd83e15d5b99691a1065a5fac28
denilany <denilany1@gmail.com>
2024-05-30 16:36:46 +0300

    chore: Change the main() function to be able to run the ListPushFront() function

--- datastructs/main.go
+++ datastructs/main.go
@@ -6,6 +6,7 @@ import (
 	"Algorithims/datastructs/linkedlist"
 )
 
+/*
 func main() {
 	link := &linkedlist.List{}
 
@@ -18,4 +19,20 @@ func main() {
 		link.Head = link.Head.Next
 	}
 	fmt.Println("nil")
+}
+*/
+
+func main() {
+	link := &linkedlist.List{}
+
+	linkedlist.ListPushFront(link, "Hello")
+	linkedlist.ListPushFront(link, "man")
+	linkedlist.ListPushFront(link, "how are you")
+
+	it := link.Head
+	for it != nil {
+		fmt.Print(it.Data, " ")
+		it = it.Next
+	}
+	fmt.Println()
 }
