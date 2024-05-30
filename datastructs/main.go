8ff71b0eaa90526a269681e8fefc2603e176c413
denilany <denilany1@gmail.com>
2024-05-30 14:26:27 +0300

    fix: Fixed the function call to resemble the actual function created

--- datastructs/main.go
+++ datastructs/main.go
@@ -9,9 +9,9 @@ import (
 func main() {
 	link := &linkedlist.List{}
 
-	linkedlist.LinkedList(link, "Hello")
-	linkedlist.LinkedList(link, "man")
-	linkedlist.LinkedList(link, "how are you")
+	linkedlist.ListPushBack(link, "Hello")
+	linkedlist.ListPushBack(link, "man")
+	linkedlist.ListPushBack(link, "how are you")
 
 	for link.Head != nil {
 		fmt.Print(link.Head.Data, " -> ")
