33cc7c3901244af81696648b3f47fc0189614175
denilany <denilany1@gmail.com>
2024-06-06 21:46:11 +0300

    fix(rot13 func): fix the function to output the correct result

--- rot13/rot13.go
+++ rot13/rot13.go
@@ -2,19 +2,24 @@ package main
 
 import "fmt"
 
-func activeBits(num int) int {
-	count := 0
+func rot13(str string) string {
+	result := ""
 
-	for i := 7; i >= 0; i-- {
-		bit := (num >> i) & 1
-		if num < bit {
-			count++
+	for _, ch := range str {
+		if ch >= 'a' && ch <= 'z' {
+			result += string((ch-'a'+13)%26 + 'a')
+		} else if ch >= 'A' && ch <= 'Z' {
+			result += string((ch-'A'+13)%26 + 'A')
+		} else {
+			result += string(ch)
 		}
 	}
-	return count
+	return result
 }
 
 func main() {
-	num := 7
-	fmt.Println(activeBits(num))
+	str := "abc"
+
+	res := rot13(str)
+	fmt.Println(res)
 }
