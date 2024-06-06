0b8e135579dde9341f94700556122ef45d007358
denilany <denilany1@gmail.com>
2024-06-06 23:22:31 +0300

    chore(inter func): add a function that prints the characters that apear on both sides of the strings

---
+++ inter/inter.go
@@ -0,0 +1,33 @@
+package main
+
+import "fmt"
+
+func inter(str1, str2 string) string {
+	map1 := make(map[rune]bool)
+	result := make([]rune, 0, len(str1))
+
+	for _, ch := range str1 {
+		if _, ok := map1[ch]; !ok && containsRune(str2, ch) {
+			map1[ch] = true
+			result = append(result, ch)
+		}
+	}
+	return string(result)
+}
+
+func containsRune(s string, r rune) bool {
+	for _, char := range s {
+		if char == r {
+			return true
+		}
+	}
+	return false
+}
+
+func main() {
+	str1 := "padinton"
+	str2 := "paqefwtdjetyiytjneytjoeyjnejeyj"
+
+	result := inter(str1, str2)
+	fmt.Println(result)
+}
