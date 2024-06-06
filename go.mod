20c8eeab6d2e7749cf0f9bddf5d73c0b57ad336f
denilany <denilany1@gmail.com>
2024-06-06 12:48:10 +0300

    chore: Add a roman ciper code that rotates letters to 13 characters ahead

--- go.mod
+++ go.mod
@@ -1,5 +1,5 @@
 module Algorithims
 
-go 1.22.1
+go 1.23
 
 require github.com/01-edu/z01 v0.1.0
---
+++ rot13/rot13.go
@@ -0,0 +1,25 @@
+package main
+
+import "fmt"
+
+func rot13(str string) string {
+	result := ""
+
+	for _, ch := range str {
+		if ch >= 'a' && ch <= 'z' {
+			result += string((ch-'a'+13)%26 + 'a')
+		} else if ch >= 'A' && ch <= 'Z' {
+			result += string((ch-'A'+13)%26 + 'A')
+		} else  {
+			result += string(ch)
+		}
+	}
+	return result
+}
+
+func main() {
+	str := "abc"
+
+	res := rot13(str)
+	fmt.Println(res)
+}
